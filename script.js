54815a5f7c9b42b1641f1f5674360a3af1f2ccc5
Ian <janero78rus@yandex.ru>
2021-01-27 21:07:19 +0300

    2 commit

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-//alert('Привет Луиза!!');
-let incr = 10,
-    decr = 10;
+let money = +prompt("ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-console.log(incr++);
-console.log(decr--);
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
 
-console.log(7%2);
+let i = 0; 
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расхода в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+i++;
+if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' 
+&& a.length < 50) {
+    console.log("done");
+    appData.expenses [a] = b;
+}
+};
 
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log ("произошла ошибка");
+}
